1427b8b9eac651cb0f446285f0dd479a036f1877
Stanislav Shestak <stastalmid@gmail.com>
2022-07-10 02:11:13 +0300

    Lesson 13

---
+++ src/components/Profile.js
@@ -0,0 +1,26 @@
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&w=1080&fit=max&q=80&fm=jpg&crop=entropy&cs=tinysrgb' alt='beachBackGround'></img>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+            </div>
+            <div>
+                New Post
+            </div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
